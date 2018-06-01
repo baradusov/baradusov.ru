25d7291f14cefc8495dad37322eae17368b1424f
baradusov <baradusovnh@gmail.com>
2018-06-01 21:10:34 +0400

    удали чат-каунтер и сделал список ссылок в работах

--- works/chat-counter.js
+++
@@ -1,8 +0,0 @@
-let counter = 0;
-const btn = document.querySelector('[data-react="123.123123.123"]');
-const div = document.querySelector('.counter');
-
-btn.addEventListener('click', () => {
-  counter += 1;
-  div.innerHTML = counter;
-});
