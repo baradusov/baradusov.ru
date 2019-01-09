8e1409409003066b1a3d4f9648928b94a9e13a7b
Нуриль Барадусов <baradusovnh@gmail.com>
2019-01-09 15:33:51 +0400

    Добавляет оповещение в слак при изменении аватара

--- src/functions/twitter.js
+++ src/functions/twitter.js
@@ -1,7 +1,7 @@
-var Twit = require('twit');
+var Twit = require("twit");
 
 exports.handler = function(event, context, callback) {
-  const image = event.queryStringParameters['image'].split(',')[1];
+  const image = event.queryStringParameters["image"].split(",")[1];
   const T = new Twit({
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
@@ -9,18 +9,45 @@ exports.handler = function(event, context, callback) {
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
   });
 
-  T.post('account/update_profile_image', {
-    image: image
-  }, (err, data, response) => {
+  const slack = {
+    first: process.env.SLACK_FIRST,
+    second: process.env.SLACK_SECOND,
+    third: process.env.SLACK_THIRD
+  };
+
+  const sendToSlack = message => {
+    fetch(
+      `https://hooks.slack.com/services/${slack.first}/${slack.second}/${
+        slack.third
+      }`,
+      {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: message
+        })
+      }
+    );
+  };
+
+  T.post(
+    "account/update_profile_image",
+    {
+      image: image
+    },
+    (err, data, response) => {
       callback(null, {
         statusCode: 200,
-        body: JSON.stringify(
-          {
-            "message": data,
-            "error": err,
-            "response": response
-          }
-        )
+        body: JSON.stringify({
+          message: data,
+          error: err,
+          response: response
+        })
       });
-  });
-}
+      sendToSlack(
+        `Аватар обновлён https://twitter.com/baradusov: ${
+          response.profile_image_url
+        }.`
+      );
+    }
+  );
+};
