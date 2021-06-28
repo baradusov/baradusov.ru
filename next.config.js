e49859595eedad5ef15335c85e09af5cf3887790
Nuril Baradusov <baradusovnh@gmail.com>
2021-06-28 19:32:34 +0400

    Обновил зависимости

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const withMDX = require('@next/mdx')({
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx'],
-  future: {
-    webpack5: true,
-  },
   webpack: (config, { dev, isServer }) => {
     if (!dev && isServer) {
       require('./scripts/generateRss');
