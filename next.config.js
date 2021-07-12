169c556a7d61f5ae5220d509052b9a5ae235886b
Nuril Baradusov <baradusovnh@gmail.com>
2021-07-12 16:30:38 +0400

    Добавил редиректы

--- next.config.js
+++ next.config.js
@@ -11,4 +11,23 @@ module.exports = withMDX({
 
     return config;
   },
+  async redirects() {
+    return [
+      {
+        source: '/auto-thoughts',
+        destination: '/posts/2018/auto-thoughts',
+        permanent: true,
+      },
+      {
+        source: '/math-and-frontend',
+        destination: '/posts/2018/math-and-frontend',
+        permanent: true,
+      },
+      {
+        source: '/accessibility-is-not-what-you-think',
+        destination: '/posts/2018/accessibility-is-not-what-you-think',
+        permanent: true,
+      },
+    ];
+  },
 });
