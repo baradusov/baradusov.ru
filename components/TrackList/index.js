c90ddba1c54ca5746ea64d5662313de1fbf63016
Nuril Baradusov <baradusovnh@gmail.com>
2021-07-19 19:14:44 +0400

    Добавил страницу с топ-треками из спотифай

---
+++ components/TrackList/index.js
@@ -0,0 +1,31 @@
+import Link from '@components/Link';
+import styles from './index.module.css';
+
+const TrackList = (props) => {
+  const { tracks } = props;
+
+  if (tracks.length === 0) return null;
+
+  return (
+    <section>
+      <ol>
+        {tracks.map((track, index) => {
+          return (
+            <li key={index}>
+              <p className={styles.title}>
+                <Link href={track.external_urls.spotify}>{track.name}</Link>
+              </p>
+              <p className={styles.meta}>
+                {track.artists.map((artist) => {
+                  return artist.name;
+                }).join(', ')}
+              </p>
+            </li>
+          );
+        })}
+      </ol>
+    </section>
+  );
+};
+
+export default TrackList;
