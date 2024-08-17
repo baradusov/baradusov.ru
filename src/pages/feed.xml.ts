4085c7021bedf43726afa1e19e066bc52c5cb689
Нуриль Барадусов <nuril.b@ati.su>
2024-08-17 10:10:52 +0300

    исправил генерацию rss контента

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -2,6 +2,11 @@ import rss from '@astrojs/rss';
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
 
+import sanitize from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
+
 export const GET: APIRoute = async (context) => {
   const blog = await getCollection('posts', ({ data }) => data.draft === false);
 
@@ -12,7 +17,9 @@ export const GET: APIRoute = async (context) => {
     trailingSlash: false,
     items: blog.map((post) => ({
       title: post.data.title,
-      description: post.body,
+      description: sanitize(parser.render(post.body), {
+        allowedTags: sanitize.defaults.allowedTags.concat(['img']),
+      }),
       pubDate: post.data.created,
       link: `/posts/${post.slug}/`,
     })),
