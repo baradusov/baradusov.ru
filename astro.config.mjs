6cf8aad86c0bcc3c05369718e90cb06286fd913e
Нуриль Барадусов <nuril.b@ati.su>
2024-08-17 18:08:50 +0300

    добавил коммент для будущего себя

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import serviceWorker from 'astrojs-service-worker';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://baradusov.ru',
-  integrations: [mdx(), serviceWorker()],
+  integrations: [
+    mdx(),
+    // если удалить плагин sw, то не забыть вручную разрегистрировать его
+    serviceWorker(),
+  ],
 });
