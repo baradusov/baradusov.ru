c5cb0ff925edb56c79808b4cfe1472598f4a9ed0
Нуриль Барадусов <nuril.b@ati.su>
2024-08-17 18:01:35 +0300

    оффлайн

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
+import serviceWorker from 'astrojs-service-worker';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://baradusov.ru',
-  integrations: [mdx()],
+  integrations: [mdx(), serviceWorker()],
 });
