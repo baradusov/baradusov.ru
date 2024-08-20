1b35298cc9c65559a4796da9917ff83d2be4546b
Нуриль Барадусов <nuril.b@ati.su>
2024-08-20 14:26:46 +0300

    убрал оффлайн

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import serviceWorker from 'astrojs-service-worker';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://baradusov.ru',
-  integrations: [
-    mdx(),
-    // если удалить плагин sw, то не забыть вручную разрегистрировать его
-    serviceWorker(),
-  ],
+  integrations: [mdx()],
 });
